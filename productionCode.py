3b4aa3ee9a0639a1194b93b4947fc06c3484b175
Marie Clancy <marie.clancy412@gmail.com>
2020-01-29 09:33:11 -0500

    initial add of count_words and first pass at happy/bad path test

--- productionCode.py
+++ productionCode.py
@@ -1,8 +1,11 @@
-import pathlib.Path
+#import pathlib.Path
+from typing import List, Dict
 
-def count_words(allLines):
+def count_words(all_lines):
     word_count = dict()
-    for line in allLines:
+    if not (type(all_lines) is list):
+            return word_count
+    for line in all_lines:
         messy_words = line.split(" ")
         for messy_word in messy_words:
             cleaner_word = messy_word.rstrip("""!@#$%^&*()-={}[]"';:,./?""")
@@ -13,13 +16,13 @@ def count_words(allLines):
                 word_count[clean_word] = 1
     return word_count
 
-def main():
-    path = pathlib.Path.cwd()/'warpeace.txt'
-    file = open(str(path))
-    all_lines = file.readlines()
-    results = count_words(all_lines)
-    for word in results:
-        print(f"{word} appears {results[word]} times")
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     path = pathlib.Path.cwd()/'warpeace.txt'
+#     file = open(str(path))
+#     all_lines = file.readlines()
+#     results = count_words(all_lines)
+#     for word in results:
+#         print(f"{word} appears {results[word]} times")
+#
+# if __name__ == '__main__':
+#     main()
--- test/testProduction.py
+++ test/testProduction.py
@@ -1,9 +1,18 @@
 import pytest
 import productionCode
-
 def test_good_list():
     test_data = ["Hi Hi Hi there you%%",
                  "There Hi RTRE$^%$  $#%$#FGF %$^Hi"]
     counts = productionCode.count_words(test_data)
     assert "Hi" in counts
     assert counts["Hi"] ==5
+
+def test_bad_data1():
+    test_data = {}
+    counts = productionCode.count_words(test_data)
+    assert len(counts) == 0
+
+def test_bad_data2():
+    test_data = 1223
+    counts = productionCode.count_words(test_data)
+    assert len(counts) == 0
